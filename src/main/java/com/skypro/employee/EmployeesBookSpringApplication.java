ccd01c9d7e3fa3bbc72520957791796eea80bebb
Admin O <olexandro@gmail.com>
2022-11-17 23:56:49 +0300

    Initial commit

---
+++ src/main/java/com/skypro/employee/EmployeesBookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBookSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesBookSpringApplication.class, args);
+	}
+
+}
