df079236e88c20de4e5619b65718dfb8170707bf
Admin O <olexandro@gmail.com>
2022-11-21 00:00:57 +0300

    Замена на стримы

--- src/main/java/com/skypro/employee/service/EmployeeService.java
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -41,34 +42,40 @@ public class EmployeeService {
     }
 
     public Employee min() {
-        int minSalary = getSalarySum();
-        Employee minEmp = null;
-        for (Map.Entry<Integer, Employee> entry :
-                employees.entrySet()) {
-            if (entry.getValue().getSalary() < minSalary) {
-                minSalary = entry.getValue().getSalary();
-                minEmp = entry.getValue();
-            }
-        }
-        return minEmp;
+//        int minSalary = getSalarySum();
+//        Employee minEmp = null;
+//        for (Map.Entry<Integer, Employee> entry :
+//                employees.entrySet()) {
+//            if (entry.getValue().getSalary() < minSalary) {
+//                minSalary = entry.getValue().getSalary();
+//                minEmp = entry.getValue();
+//            }
+//        }
+//        return minEmp;
+        return employees.values().stream()
+                .min(Comparator.comparing(Employee::getSalary))
+                .orElse(null);
     }
 
     public Employee max() {
-        int max = 0;
-        Employee maxEmp = null;
-        for (Map.Entry<Integer, Employee> entry :
-                employees.entrySet()) {
-            if (entry.getValue().getSalary() > max) {
-                max = entry.getValue().getSalary();
-                maxEmp = entry.getValue();
-            }
-        }
-        return maxEmp;
+//        int max = 0;
+//        Employee maxEmp = null;
+//        for (Map.Entry<Integer, Employee> entry :
+//                employees.entrySet()) {
+//            if (entry.getValue().getSalary() > max) {
+//                max = entry.getValue().getSalary();
+//                maxEmp = entry.getValue();
+//            }
+//        }
+//        return maxEmp;
+        return employees.values().stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .orElse(null);
     }
 
     public List<Employee> highSalary() {
         List<Employee> employeeList = new ArrayList<>();
-        float medium = mediumSalary();
+        double medium = mediumSalary();
         for (Map.Entry<Integer, Employee> entry :
                 employees.entrySet()) {
             if (entry.getValue().getSalary() > medium) {
@@ -78,12 +85,14 @@ public class EmployeeService {
         return employeeList;
     }
 
-    private float mediumSalary() {
-        int sum = 0;
-        for (Map.Entry<Integer, Employee> entry:
-                employees.entrySet()) {
-            sum += entry.getValue().getSalary();
-        }
-        return sum / employees.size();
+    private double mediumSalary() {
+//        int sum = 0;
+//        for (Map.Entry<Integer, Employee> entry:
+//                employees.entrySet()) {
+//            sum += entry.getValue().getSalary();
+//        }
+//        return sum / employees.size();
+        return employees.values().stream()
+                .collect(Collectors.averagingDouble(Employee::getSalary));
     }
 }
